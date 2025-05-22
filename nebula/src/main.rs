067bc884221b760f6e3eaaee4ec93b6cf496ecf6
EniV90 <eni_v90@yahoo.com>
2025-05-22 20:17:49 +0100

    Batman: Implementing server part 1

--- nebula/src/main.rs
+++ nebula/src/main.rs
@@ -9,32 +9,59 @@ mod protocol;
 
 
 
-// fn look_up(qname: &str, qtype: QueryType, server: (IpAddr, u16)) -> Result<DnsPacket, Box<dyn std::error::Error>> {
-//   let qname = "google.com";
-//   let qtype = QueryType::A;
+fn look_up(qname: &str, qtype: QueryType, ) -> Result<DnsPacket, Box<dyn std::error::Error>> {
+  // let qname = "google.com";
+  // let qtype = QueryType::A;
 
-//   let server = ("8.8.8.8", 53);
+  let server = ("8.8.8.8", 53);
 
-//   let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
+  let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
 
-//   let mut packet = DnsPacket::new();
+  let mut packet = DnsPacket::new();
 
-//   packet.header.id = 6666;
-//   packet.header.questions = 1;
-//   packet.header.recursion_desired = true;
+  packet.header.id = 6666;
+  packet.header.questions = 1;
+  packet.header.recursion_desired = true;
 
-//   packet.questions.push(DnsQuestion::new(qname.to_string(), qtype));
+  packet.questions.push(DnsQuestion::new(qname.to_string(), qtype));
 
-//   let mut req_buffer = BytePacketBuffer::new();
-//   let _ = packet.write(&mut req_buffer);
-//   socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
+  let mut req_buffer = BytePacketBuffer::new();
+  let _ = packet.write(&mut req_buffer);
+  socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
 
-//   let mut res_buffer = BytePacketBuffer::new();
-//   socket.recv_from(&mut res_buffer.buf)?;
+  let mut res_buffer = BytePacketBuffer::new();
+  socket.recv_from(&mut res_buffer.buf)?;
 
-//   DnsPacket::from_buffer(&mut res_buffer)
+  DnsPacket::from_buffer(&mut res_buffer)
 
-// }
+}
+
+fn handle_query(socket: &UdpSocket) -> Result<(), Box<dyn std::error::Error>> {
+  let mut req_buffer =BytePacketBuffer::new();
+
+  let(_, src) = socket.recv_from(&mut req_buffer.buf)?;
+
+  let mut request = DnsPacket::from_buffer(&mut req_buffer)?;
+
+  let mut packet =DnsPacket::new();
+  packet.header.id = request.header.id;
+  packet.header.recursion_desired = true;
+  packet.header.recursion_desired = true;
+  packet.header.response = true;
+
+
+  if let Some(question) = request.questions.pop() {
+    println!("Received query {:?}", question);
+
+    if let Ok(result) = look_up(&question.name, question.qtype) {
+    packet.questions.push(question);
+    packet.header.rescode = result.header.rescode;
+    }
+  }
+
+
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
  response_packet()?;
--- nebula/src/protocol/response_packet.rs
+++ nebula/src/protocol/response_packet.rs
@@ -7,34 +7,34 @@ use super::{byte_packet::BytePacketBuffer, dns_packet::DnsPacket};
 
 
 pub fn response_packet() -> Result<(), Box<dyn Error>> {
-  let qname = "yahoo.com";
-  let qtype = QueryType::MX;
+  // let qname = "yahoo.com";
+  // let qtype = QueryType::MX;
 
-  let server = ("8.8.8.8", 53);
+  // let server = ("8.8.8.8", 53);
 
-  let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
+  // let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
 
-  let mut packet = DnsPacket::new();
+  // let mut packet = DnsPacket::new();
 
-  packet.header.id = 6666;
-  packet.header.questions = 1;
-  packet.header.recursion_desired = true;
+  // packet.header.id = 6666;
+  // packet.header.questions = 1;
+  // packet.header.recursion_desired = true;
 
-  packet.questions.push(DnsQuestion::new(qname.to_string(), qtype));
+  // packet.questions.push(DnsQuestion::new(qname.to_string(), qtype));
 
-  let mut req_buffer = BytePacketBuffer::new();
-  packet.write(&mut req_buffer);
-  socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
+  // let mut req_buffer = BytePacketBuffer::new();
+  // packet.write(&mut req_buffer);
+  // socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
 
-  let mut res_buffer = BytePacketBuffer::new();
-  socket.recv_from(&mut res_buffer.buf)?;
+  // let mut res_buffer = BytePacketBuffer::new();
+  // socket.recv_from(&mut res_buffer.buf)?;
 
   
-  // let mut f = File::open("response_packet.txt")?;
-  // let mut buffer = BytePacketBuffer::new();
-  // let _ = f.read(&mut buffer.buf)?;
+  let mut f = File::open("response_packet.txt")?;
+  let mut buffer = BytePacketBuffer::new();
+  let _ = f.read(&mut buffer.buf)?;
 
-  let packet = DnsPacket::from_buffer(&mut res_buffer)?;
+  let packet = DnsPacket::from_buffer(&mut buffer)?;
   println!("{:#?}", packet.header);
 
   for q in packet.questions {
